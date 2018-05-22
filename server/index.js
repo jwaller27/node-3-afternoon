8a758904943fed0ae96df6dded1107ca73283676
Jonathan Waller <waller.j27@gmail.com>
2018-05-22 18:06:51 -0500

    ongoing

--- server/index.js
+++ server/index.js
@@ -3,9 +3,16 @@ const bodyParser = require("body-parser");
 const session = require("express-session");
 require("dotenv").config();
 
+// Middleware
 const checkForSession = require("./middlewares/checkForSession");
 
+//Controllers
+const swag_controller = require("./controllers/swag_controller");
+const auth_controller = require("./controllers/auth_controller");
+const cart_controller = require("./controllers/cart_controller");
+
 const app = express();
+
 app.use(bodyParser.json());
 app.use(
   session({
@@ -14,8 +21,21 @@ app.use(
     saveUninitialized: true
   })
 );
-
 app.use(checkForSession);
+
+//Swag
+app.get("/api/swag", swag_controller.read);
+
+// Auth
+app.post("/api/login", auth_controller.login);
+app.post("/api/register", auth_controller.register);
+app.post("/api/signout", auth_controller.signout);
+app.get("/api/user", auth_controller.getUser);
+
+// Cart
+app.post("/api/cart", cart_controller.add);
+app.post("/api/cart/checkout", cart_controller.checkout);
+app.delete("/api/cart", cart_controller.delete);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
